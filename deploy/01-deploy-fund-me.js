744e93cb7f1343b73dc4b00775cad5ce99fe2a39
xiaohao-gong <love819099119>
2025-01-23 00:38:56 +0800

    commit

---
+++ deploy/01-deploy-fund-me.js
@@ -0,0 +1,11 @@
+function deployFunction(){
+    console.log("this is a deploy function")
+}
+module.exports.default=deployFunction
+module.exports = async() => {
+    const getNameAccoount = getNamedeAccounts
+    const deployments = Hre.deployments
+    console.log("this is a deploy function")
+
+    console.log("this is a deploy function")
+}
